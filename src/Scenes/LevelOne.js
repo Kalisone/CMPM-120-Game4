2b1db98461efb0e3b05d6aaad0af637af07533c0
Ethan Morelos <etmorelo@ucsc.edu>
2025-06-10 16:51:17 -0700

    various bug fixes:  - player could sometimes lose two lives on death  - made debug controls only accessible with debug control [D]

--- src/Scenes/LevelOne.js
+++ src/Scenes/LevelOne.js
@@ -279,22 +279,30 @@ class LevelOne extends Phaser.Scene {
 
         // Decrement Life
         this.input.keyboard.on('keydown-MINUS', () => {
-            this.respawn(my.sprite.player, true);
+            if(this.physics.world.drawDebug){
+                this.respawn(my.sprite.player, true);
+            }
         });
 
         // Increment Life
         this.input.keyboard.on('keydown-PLUS', () => {
-            my.text.lives.setText("Lives Remaining: " + ++my.sprite.player.lives);
+            if(this.physics.world.drawDebug){
+                my.text.lives.setText("Lives Remaining: " + ++my.sprite.player.lives);
+            }
         });
 
         // Decrement Keys Remaining
         this.input.keyboard.on('keydown-NINE', () => {
-            my.text.keys.setText("Keys Remaining: " + --this.numKeys);
+            if(this.physics.world.drawDebug){
+                my.text.keys.setText("Keys Remaining: " + --this.numKeys);
+            }
         });
 
         // Increment Keys Remaining
         this.input.keyboard.on('keydown-ZERO', () => {
-            my.text.keys.setText("Keys Remaining: " + ++this.numKeys);
+            if(this.physics.world.drawDebug){
+                my.text.keys.setText("Keys Remaining: " + ++this.numKeys);
+            }
         });
     }
 
@@ -446,15 +454,14 @@ class LevelOne extends Phaser.Scene {
     }
 
     respawn(player, debug){
-        player.lives--;
-        
         if(player === my.sprite.player){
-            my.text.lives.setText("Lives Remaining: " + my.sprite.player.lives);
+            my.text.lives.setText("Lives Remaining: " + --player.lives);
         }
-
+        
         if(!debug){
             player.x = this.spawnPt.x;
             player.y = this.spawnPt.y;
+            player.body.setVelocity(0, 0);
 
             for(let sound of my.sfx.death){
                 sound.play();
